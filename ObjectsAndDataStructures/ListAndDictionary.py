cb9955cc825237edc910d8b2d08d1e609ac18230
SinghJitender <Jitu.singh95@gmail.com>
2020-03-04 18:28:53 +0530

    ListAndDictionary - Lists inbuilt methods

--- ObjectsAndDataStructures/ListAndDictionary.py
+++ ObjectsAndDataStructures/ListAndDictionary.py
@@ -12,3 +12,15 @@ print(list[1:]) # All items starting from index 1
 print(len(list)) # length of list
 list += [6,7,8] # adding elements/ concatenating list to another list
 print(list)
+
+# Python provides alot of inbuilt methods such as sort(), reverse(), pop(), append()
+list.sort()
+print(list) # prints sorted list. sort() method do not return anything, it sorts the original list
+list.reverse()
+print(list)
+list.append(9) # adds element to the end of list
+print(list)
+item1 = list.pop() # removes last element
+item2 = list.pop(0) # remove element with the index provided. By default its -1;
+print(f"{item1} and {item2}")
+print(list)
