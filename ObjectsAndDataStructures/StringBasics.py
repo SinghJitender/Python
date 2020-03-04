74426b15d5a110942923c12c6e734aeedcd9c2e9
SinghJitender <Jitu.singh95@gmail.com>
2020-03-04 14:44:42 +0530

    String Basics - Slicing/SubString in string

--- ObjectsAndDataStructures/StringBasics.py
+++ ObjectsAndDataStructures/StringBasics.py
@@ -9,10 +9,18 @@ print(a+" "+b)
 a = len(b)
 print(a)
 
-# indexing in string. Character at specific index can be retrived by using [index] after the string. Example below
+# indexing in string. Character at specific index can be retrieved by using [index] after the string. Example below
 
 string = "I am a Developer"
 print(string[0]) #Prints - I
 # We can also use negative index to print characters from end of string,
 # meaning index = -1 will represent last character and so on
 print(string[-1]) #Prints - r
+
+# substrings can be retrieved by passing starting and end index in [] seperated by : after the string
+# example - stringName[startIndex:EndIndex]
+# Note - value at endindex is not included in the string.
+secondString = "abcdefghij";
+print(secondString[2:]) # print string starting from index 2 up till end of string
+print(secondString[:4]) # print string starting from index 0 up till index 4 (excluding value at index 4 i.e 'e')
+print(secondString[3:6]) # print string starting from index 3 up till index 5 (excluding 6)
