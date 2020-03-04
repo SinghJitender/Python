4cdc3ae3737e994f09c8dc6225da51b4e08acadd
SinghJitender <Jitu.singh95@gmail.com>
2020-03-04 14:35:11 +0530

    String Basics - Indexing in string

--- ObjectsAndDataStructures/StringBasics.py
+++ ObjectsAndDataStructures/StringBasics.py
@@ -8,3 +8,11 @@ print(a+" "+b)
 # len() function to find length of string
 a = len(b)
 print(a)
+
+# indexing in string. Character at specific index can be retrived by using [index] after the string. Example below
+
+string = "I am a Developer"
+print(string[0]) #Prints - I
+# We can also use negative index to print characters from end of string,
+# meaning index = -1 will represent last character and so on
+print(string[-1]) #Prints - r
