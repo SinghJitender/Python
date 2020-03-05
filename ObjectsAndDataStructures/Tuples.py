d00c68aeaa65fd667bf8494d3c9ec2f7933677c4
jitu.singh95@gmail.com <create@new@password>
2020-03-05 15:06:27 +0530

    Tuple : Basic tuple

---
+++ ObjectsAndDataStructures/Tuples.py
@@ -0,0 +1,5 @@
+# Tuples are very similar to lists but they are immutable. i.e once a value is assigned it cannot be changed.
+# Tuples are defined in brackets " (  ) "
+
+tup = (1,2,3,4,5)
+print(tup)
