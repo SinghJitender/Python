c9ef15ef77c1001e54e67ed133e6d5c70fd67faf
SinghJitender <jitu.singh95@gmail.com>
2020-03-06 16:47:14 +0530

    ListAndDictionary - Tuples - Basics

--- ObjectsAndDataStructures/Tuples.py
+++ ObjectsAndDataStructures/Tuples.py
@@ -3,3 +3,10 @@
 
 tup = (1,2,3,4,5)
 print(tup)
+
+# tuples can also contains any type of element
+tup2 = ("One",2,3.00)
+print(tup2)
+
+# tuples cannot be reassigned with any value like in list
+# Example - tup2[0] = "1" will give an error
