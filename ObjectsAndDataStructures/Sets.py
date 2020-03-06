1d23de6c79d9a9b11c7afcb3c1a451997bcd8d11
SinghJitender <Jitu.singh95@gmail.com>
2020-03-06 17:06:37 +0530

    ListAndDictionary - Set - Basics

---
+++ ObjectsAndDataStructures/Sets.py
@@ -0,0 +1,11 @@
+# sets is an unordered collection of unique objects
+
+myset = set() # creates empty set
+print(myset)
+myset.add(1)
+myset.add(2)
+print(myset)
+
+# add list to set
+mylist = [1,1,1,1,2,2,2,3,3,3]
+print(set(mylist))
