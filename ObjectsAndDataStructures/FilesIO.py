607cdadec2ee6b6533b0b1764387ec517ec3e919
SinghJitender <Jitu.singh95@gmail.com>
2020-03-07 17:58:31 +0530

    ListAndDictionary - FilesIO - Opening file in different modes

--- ObjectsAndDataStructures/FilesIO.py
+++ ObjectsAndDataStructures/FilesIO.py
@@ -15,3 +15,11 @@ with open('test') as myfile:
     contents = myfile.read()
 # using the above syntax it automaticallys closes the file object once you are out of the given code block
 print(contents)
+
+# file is always open in read mode by default. to write to a file we have to open it in write mode.
+# we can pass the 'mode' is 'open()' method to open the file in specific mode like we can pass 'w' for writing , 'a' for appending, etc
+# all mode - 'r' , 'w', 'a', 'r+' (reading and writing), 'w+' (writing and reading. Either overwrites existing file or create new file )
+
+with open('test','w') as myfile:
+    myfile.write(contents + "This file is overwritten.")
+
