f9b22fa0045d6c7e9959dc2495798a8d186ca911
SinghJitender <Jitu.singh95@gmail.com>
2020-03-08 15:14:39 +0530

    Comparsion_Operators - LogicalOperators

---
+++ Comparsion Operators/OperatorChainingAndLogicalOperators.py
@@ -0,0 +1,9 @@
+# Comparsion can be used as a chain.
+print(4>3>2>1)
+print(4>3==3>1)
+
+# Logical operators can be used to combine two or more comparsion operators - Basic 'and', 'or', 'not'
+
+print( 3==3 and 4>3)
+print( 3!=3 or 4==4)
+print( not 2>1)
