2c78249c7767728480cd8478482acf9de12c2b1d
SinghJitender <Jitu.singh95@gmail.com>
2020-03-08 14:35:13 +0530

    Comparsion_Operators - ComparsionOperators

---
+++ Comparsion Operators/ComparsionOperator.py
@@ -0,0 +1,9 @@
+# All comparsion operators == , != , < , > , >= , <=
+print(1>2)
+print(2==2)
+print(4<=1)
+print(2>=1)
+print(2.0 == 2)
+print(3!=3)
+print('bye' == 'Bye')
+print('Python' == 'Python')
