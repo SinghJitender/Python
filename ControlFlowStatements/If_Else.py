f2722145558172b10104aa7978ed58cdff4b4dfa
jitu.singh95@gmail.com <create@new@password>
2020-03-09 12:50:17 +0530

    ControlFlow - Simple if/else/elif

--- ControlFlowStatements/If_Else.py
+++ ControlFlowStatements/If_Else.py
@@ -5,3 +5,12 @@ if boolean:
     print("It's true!");
 else:
     print("It's False!")
+
+
+loc = "Bank"
+if loc == "Yes":
+    print("Yes")
+elif loc == "BOI":
+    print("BOI")
+else:
+    print("Bank")
