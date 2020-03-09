e9c2519d64d795e9da04bedbc0a0a7d07d8227ad
SinghJitender <jitu.singh95@gmail.com>
2020-03-09 17:39:43 +0530

    ControlFlow - Comprehensions - Complex uses

--- ControlFlowStatements/Comprehensions.py
+++ ControlFlowStatements/Comprehensions.py
@@ -15,3 +15,14 @@ print(mylist2)
 # other uses - Get a list with every number in range's square root
 mylist3 = [num**2 for num in range(0,10)]
 print(mylist3)
+
+# complex uses - using if statement along this
+mylist4 = [num for num in range(0,20) if(num%2==0)]
+print(mylist4)
+# else can also be used but the order changes - Example
+mylist5 = [x if x%2 == 0 else 'ODD' for x in range(0,10)]
+print(mylist5)
+
+# nested loop in list comprehensions
+mylist6 = [x*y for x in [2,4,6] for y in [1,10,100]]
+print(mylist6)
