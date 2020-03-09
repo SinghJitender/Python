dbaff2e91df3677388191327bcc3358243848579
SinghJitender <jitu.singh95@gmail.com>
2020-03-09 14:20:48 +0530

    ControlFlow - UsefulOperators - range()

---
+++ ControlFlowStatements/UsefulOperators.py
@@ -0,0 +1,8 @@
+# range(start,end,step)
+# range function can be used to generate number with a specific range
+
+for num in range(0,10,1):
+    print(num)
+
+mylist = list(range(0,20,2)) # create a list including numbers uptill 19 with stepsize 2.
+print(mylist)
