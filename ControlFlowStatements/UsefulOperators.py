58dc465c65f1f137209f2885ec0247f2da787af7
SinghJitender <jitu.singh95@gmail.com>
2020-03-09 16:52:33 +0530

    ControlFlow - UsefulOperators - enumerate()

--- ControlFlowStatements/UsefulOperators.py
+++ ControlFlowStatements/UsefulOperators.py
@@ -6,3 +6,12 @@ for num in range(0,10,1):
 
 mylist = list(range(0,20,2)) # create a list including numbers uptill 19 with stepsize 2.
 print(mylist)
+
+# enumerate() can be used to create a tuple for index and value
+word ="abcdef"
+
+for item in enumerate(word):
+    print(item)
+
+for index,item in enumerate(word):
+    print(f"{index} : {item}")
