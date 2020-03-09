11957b2d5ccaebc0ff23d47453065008f5166d47
SinghJitender <jitu.singh95@gmail.com>
2020-03-09 14:08:17 +0530

    ControlFlow - While loop - break,pass,continue

--- ControlFlowStatements/WhileLoop.py
+++ ControlFlowStatements/WhileLoop.py
@@ -1,8 +1,19 @@
 # while statement can be used with else in python
 
 x = 0
-while x<5:
+while x < 5:
     print(x)
-    x+=1
+    x += 1
 else:
     print("x is not equal to 5")
+
+# break, continue, pass
+# pass - do nothing at all
+x=0
+while x < 10:
+    if x == 9:
+        print("Printing")
+        break
+    print(x)
+    x += 1
+print("Out of loop now")
