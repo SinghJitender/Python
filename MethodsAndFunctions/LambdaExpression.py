1b217136b10d3d782c0a0c35996824198a8887df
SinghJitender <Jitu.singh95@gmail.com>
2020-03-10 18:16:56 +0530

    MethodsAndFunctions - LambdaExpression - map() - description added

--- MethodsAndFunctions/LambdaExpression.py
+++ MethodsAndFunctions/LambdaExpression.py
@@ -1,5 +1,7 @@
 # map() and filter()
 # lambda expression are anonymous functions
+
+# map() is used to map each item in the list to the given function and returns a list
 def sqrt(num):
     return num**2
 mylist = [1,2,3,4,5]
