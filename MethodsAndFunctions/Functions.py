9cd9d63e6bd2b2dbb19c4aee229035e5e3993b9c
SinghJitender <Jitu.singh95@gmail.com>
2020-03-10 14:44:23 +0530

    MethodsAndFunctions - Functions - Basic function

---
+++ MethodsAndFunctions/Functions.py
@@ -0,0 +1,13 @@
+# Funtions can be used as a placeholder for a code which will be used multiple times
+# Functions are declared in following way
+# def function_name(parameters):
+#   body of function
+
+# Functions can be of two types - Returning funtion and Non returning function
+
+# Non Returning functions - Does not return anything
+
+def print_name(name):
+    print("Hello "+name)
+
+print_name("Jitender")
