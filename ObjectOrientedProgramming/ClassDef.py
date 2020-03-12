8fdff5003d123505733cecd860228f1d28396a43
SinghJitender <jitu.singh95@gmail.com>
2020-03-12 15:29:41 +0530

    ObjectOrientedProgramming -Class Definition Example

---
+++ ObjectOrientedProgramming/ClassDef.py
@@ -0,0 +1,12 @@
+# Python implements Object Oriented Programming using CLASSES
+# Classes can be defined using following index
+class className:
+
+    def __init__(self,param):
+        self.param = param
+
+    def someMethod(self):
+        print(self.param)
+
+#end of class
+
