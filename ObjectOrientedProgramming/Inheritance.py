a08ecf420b7ecd659ec7ca0fd3d4b49e86ed2585
SinghJitender <Jitu.singh95@gmail.com>
2020-03-15 17:29:13 +0530

    MethodsAndFunctions - Inheritance - Initial Commit

---
+++ ObjectOrientedProgramming/Inheritance.py
@@ -0,0 +1,24 @@
+# classes can be extended to implement inhertance
+
+class Animal:
+    def __init__(self):
+     print("Animal Created")
+
+    def who_am_i(self):
+     print("I'm an animal")
+
+    def eat(self):
+        print("I'm eating")
+
+# Class Dog extends Class Animal and all its methods (Animal Class) can now be called using Dog class object
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog Created")
+
+    def brak(self):
+        print("Barking!!")
+
+    # Method Overriding
+    def who_am_i(self):
+        print("I'm a Dog.")
