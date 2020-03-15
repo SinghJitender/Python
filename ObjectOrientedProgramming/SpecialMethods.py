ba2b09f6bc694283116a879c681c0597c694fc24
SinghJitender <Jitu.singh95@gmail.com>
2020-03-15 18:00:14 +0530

    MethodsAndFunctions - Special Methods - __str__ (toString() of JAVA)

--- ObjectOrientedProgramming/SpecialMethods.py
+++ ObjectOrientedProgramming/SpecialMethods.py
@@ -1,1 +1,18 @@
 # We will see how we can iplement or use Python's inbuilt methods in our own class like - print, len, etc
+
+class Book():
+    def __init__(self,author,book,pages):
+        self.author = author
+        self.book = book
+        self.pages = pages
+
+    # toString method of Python. Returns String representation of class
+    def __str__(self):
+        return f"{self.author} by {self.book}"
+
+book1  = Book("Jitender","Python Advance to Null",200)
+# __str__ method being called automatically
+print(book1)
+
+
+
