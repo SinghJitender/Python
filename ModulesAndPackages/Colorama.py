fb99e5690a4b362379f113d53758b7399d0031ed
SinghJitender <Jitu.singh95@gmail.com>
2020-03-16 14:34:28 +0530

    ModulesAndPackages - Colorama - How to use?

---
+++ ModulesAndPackages/Colorama.py
@@ -0,0 +1,7 @@
+# Find Custom package in python at : https://pypi.org/
+
+from colorama import init
+init()
+from colorama import Fore
+print(Fore.RED+"This is in red colour")
+print(Fore.GREEN+"This is in Green colour")
