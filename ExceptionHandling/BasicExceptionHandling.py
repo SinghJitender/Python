1a7c4d24e577abbeb840379169ae413bffabcc38
SinghJitender <Jitu.singh95@gmail.com>
2020-03-17 12:13:13 +0530

    ExceptionHandling - try-except, finally - info

--- ExceptionHandling/BasicExceptionHandling.py
+++ ExceptionHandling/BasicExceptionHandling.py
@@ -8,6 +8,7 @@
 #   else-block-code
 
 # if try block is executed successfully. Then the control goes to else block. If not then except block is executed but not else block
+# else block is not necessary. try-expect will also work
 
 def divide(a,b):
     return a/b
@@ -29,3 +30,12 @@ except:
     print("Exception Occured : ")
 else:
     print("This is else block")
+
+# Example 3
+try:
+    result = divide(2/4)
+    print("Result is {}".format(result))
+except:
+    print("Error Occured.")
+
+# If something else needs to be executed after the except block then 'finally block should be used.
