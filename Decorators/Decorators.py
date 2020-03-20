6c3e64573f103fadaa05b9d789871bffa0660b5b
SinghJitender <Jitu.singh95@gmail.com>
2020-03-20 16:00:45 +0530

    Decorators - Function inside function

--- Decorators/Decorators.py
+++ Decorators/Decorators.py
@@ -4,3 +4,16 @@ def hello():
 
 x = hello
 x()
+
+# Internal Functions (Function inside function)
+# They can only be invoked inside the original/parent function
+
+def func1():
+    print("Inside function 1")
+    def func2():
+        print("Inside function 2")
+    def func3():
+        print("Inside function 3")
+    print("End of function 1")
+
+func1()
