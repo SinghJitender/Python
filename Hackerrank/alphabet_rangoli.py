c2423c120cd0747b264698635bd9878904693922
SinghJitender <Jitu.singh95@gmail.com>
2020-04-01 16:38:32 +0530

    Hackerrank  - alphabet_rangoli

---
+++ Hackerrank/alphabet_rangoli.py
@@ -0,0 +1,98 @@
+'''
+You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:
+
+#size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+Input Format
+
+Only one line of input containing , the size of the rangoli.
+
+Constraints
+
+
+Output Format
+
+Print the alphabet rangoli in the format explained above.
+
+Sample Input
+
+5
+Sample Output
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+'''
+
+
+def print_rangoli(size):
+    # your code goes here
+    ch = chr(size + 96)
+    pattern = ""
+    temp = ""
+    templist = list()
+    maxlen = int((((size * 2) - 1) * 2) - 1)
+    for i in range(1, size + 1):
+        pattern = pattern + ch
+        output = "-".join(list(pattern + temp[::-1]))
+        result = str(f'{int((maxlen - len(output)) / 2) * "-"}{output}{int((maxlen - len(output)) / 2) * "-"}')
+        templist.append(result)
+        print(result)
+        temp = pattern[0:i]
+        ch = chr(size + 96 - i)
+    for x in range(len(templist) - 2, -1, -1):
+        print(templist[x])
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
