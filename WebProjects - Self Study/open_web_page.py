310f1a7e5645e48941746eaa8b5d38a11bd72003
SinghJitender <Jitu.singh95@gmail.com>
2020-04-08 16:23:40 +0530

    WebProjects - open_web_page - read web page

--- WebProjects - Self Study/open_web_page.py
+++ WebProjects - Self Study/open_web_page.py
@@ -1,3 +1,8 @@
 # Open file in web browser
-import webbrowser
-webbrowser.open('http://nseindia.com')
+#import webbrowser
+#webbrowser.open('http://nseindia.com')
+
+#read webpage
+import urllib.request
+page = urllib.request.urlopen('https://www.google.com/')
+print(page.read())
