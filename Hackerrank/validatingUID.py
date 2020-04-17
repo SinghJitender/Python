1d68a38313cd67efa176dede69a8e83bb117b4fd
SinghJitender <Jitu.singh95@gmail.com>
2020-04-17 16:08:42 +0530

    Hackerrank - validatingUID - solution

--- Hackerrank/validatingUID.py
+++ Hackerrank/validatingUID.py
@@ -34,3 +34,34 @@ B1CD102354:  is repeating → Invalid
 B1CDEF2354: Valid
 '''
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num = "0123456789"
+smallAplha = "abcdefghijklmnopqrstuvwxyz"
+bigAplha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for x in range(int(input())):
+    s = input()
+    digits = 0
+    uppercasechar = 0
+    partials = ""
+    if(len(s)==10):
+        flag = True
+        for ch in s:
+            if(ch in partials):
+                print("Invalid")
+                flag = False
+                break
+            elif(ch in num):
+                digits+=1
+            elif(ch in bigAplha):
+                uppercasechar +=1
+            elif(ch not in bigAplha and ch not in smallAplha and ch not in num):
+                print("Invalid")
+                flag = False
+                break
+            partials += ch
+        if (digits>=3 and uppercasechar>=2 and flag == True):
+             print("Valid")
+        elif ( (digits<3 or uppercasechar<2 ) and flag == True ):
+            print("Invalid")
+    else :
+        print("Invalid")
